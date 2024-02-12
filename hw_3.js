76c2ea1bfd1e1073c75e6217c3fe65b5cb8d464c
gjgjgfr333 <tabakevgenijj@gmail.com>
2024-02-12 18:33:02 +0200

    испровил

--- hw_3.js
+++ hw_3.js
@@ -1,12 +1,17 @@
 function  getTimeOfDay (num){
     if (num >= 0 && num <=5){
         return 'ночь'
-    }else if (num >5 && num <=16){
+    }else if (num >=13 && num <=16){
         return 'день'
     }else if (num >=17 && num <=24){
         return 'вечер'
-    }else {
+    }else if (num >5 && num <=12){
+        return 'утро'
+    }
+    else {
         return 'неправильно число'
     }
 }
-console.log(getTimeOfDay(37))
+console.log(getTimeOfDay(13
+))
+// лучше наверное переписать под swith case
